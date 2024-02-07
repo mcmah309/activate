812a7e4ef509e1445e7edc3aa7557fdfbbc51281
mcmah309 <dillonmcmahon79@gmail.com>
2024-02-07 13:42:23 +0000

    feat: Activate env file

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ struct Activate {
 
 const ACTIVATE_TOML: &'static str = "activate.toml";
 const ACTIVATE_DIR: &'static str = ".activate";
+const ACTIVATE_STATE_DIR: &'static str = "state";
 const ENV_FILE: &'static str = "env.json";
 
 fn main() {
@@ -44,16 +45,21 @@ fn main() {
         .expect(&format!("'{}' is not a valid environment", &selected_env));
 
     let activate_current_dir = Path::new(ACTIVATE_DIR);
-    if activate_current_dir.exists() {
-        decativate_old_env(&activate_current_dir);
+    let state_dir = activate_current_dir.join(ACTIVATE_STATE_DIR);
+    if state_dir.exists() {
+        decativate_current(&state_dir);
     } else {
-        fs::create_dir(activate_current_dir)
-            .expect(&format!("Could not create `{}` directory.", ACTIVATE_DIR));
+        fs::create_dir_all(&state_dir).expect(&format!(
+            "Could not create `{}` directory.",
+            state_dir.to_string_lossy()
+        ));
+        create_gitignore_file(&state_dir);
     }
 
-    let activate_env_file = fs::File::create(activate_current_dir.join(ENV_FILE))
-        .expect(&format!("Could not create `{}` file.", ENV_FILE));
-    add_env_vars(&env, &activate_env_file);
+    let activate_env_file =
+        fs::File::create(activate_current_dir.join(ACTIVATE_STATE_DIR).join(ENV_FILE))
+            .expect(&format!("Could not create `{}` file.", ENV_FILE));
+    add_env_file(&env, &activate_env_file);
 }
 
 #[derive(Debug, Deserialize)]
@@ -67,34 +73,19 @@ struct EnvironmentData {
 #[derive(Debug, Serialize, Deserialize)]
 struct ActiveEnvironmentEnv(HashMap<String, String>);
 
-fn decativate_old_env(activate_current_dir: &Path) {
-    let activate_env_file = activate_current_dir.join(ENV_FILE);
+fn decativate_current(activate_state_dir: &Path) {
+    let activate_env_file = activate_state_dir.join(ENV_FILE);
     if !activate_env_file.exists() {
         return;
     }
-    let contents = fs::read_to_string(&activate_env_file)
-        .expect(&format!("Could not read `{}` file.", ENV_FILE));
 
-    let current_env: ActiveEnvironmentEnv =
-        serde_json::from_str(&contents).expect(&format!("Could not parse `{}`.", ENV_FILE));
-
-    remove_env_vars(&current_env.0);
     fs::remove_file(&activate_env_file).expect(&format!(
-        "Could not remove `{}` file, but environment variables were deactivated. Please remove it manually.",
+        "Could not remove `{}` file. Environemnt is still active.",
         ENV_FILE
     ));
 }
 
-fn remove_env_vars(env_vars: &HashMap<String, String>) {
-    for (key, _value) in env_vars {
-        env::remove_var(key);
-    }
-}
-
-fn add_env_vars(env_vars: &HashMap<String, String>, mut activate_current_file: &File) {
-    for (key, value) in env_vars {
-        env::set_var(key, value);
-    }
+fn add_env_file(env_vars: &HashMap<String, String>, mut activate_current_file: &File) {
     activate_current_file
         .write(
             serde_json::to_string(&ActiveEnvironmentEnv(env_vars.clone()))
@@ -102,4 +93,9 @@ fn add_env_vars(env_vars: &HashMap<String, String>, mut activate_current_file: &
                 .as_bytes(),
         )
         .expect(&format!("Could not write to `{}` file.", ENV_FILE));
+}
+
+
+fn create_gitignore_file(state_dir: &Path) {
+    fs::write(&state_dir.join(".gitignore"), "*").expect("Could not create `.gitignore` file.");
 }
