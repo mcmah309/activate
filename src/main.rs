baea8d7af8610a77bc36f42d56a0704bf8a8457c
mcmah309 <dillonmcmahon79@gmail.com>
2024-02-07 03:15:57 +0000

    feat: Working creating env

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use clap::Parser;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::{
-    collections::HashMap, fs::{self, File}, io::{BufReader, Read}, iter::Map, path::Path
+    collections::HashMap,
+    env,
+    fs::{self, File},
+    io::{BufReader, Read, Write},
+    iter::Map,
+    path::Path,
 };
 use toml::{de, Value};
 
@@ -14,60 +19,87 @@ struct Activate {
     env_name: String,
 }
 
+const ACTIVATE_TOML: &'static str = "activate.toml";
+const ACTIVATE_DIR: &'static str = ".activate";
+const ENV_FILE: &'static str = "env.json";
+
 fn main() {
-    let activate_file = Path::new("activate.toml");
+    let activate_file = Path::new(ACTIVATE_TOML);
     if !activate_file.exists() {
-        panic!("No `activate.toml` file found in the current directory.");
+        panic!(
+            "No `{}` file found in the current directory.",
+            ACTIVATE_TOML
+        );
     }
-    let file = File::open(activate_file).expect("Could not open `activate.toml` file.");
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader
-        .read_to_string(&mut contents)
-        .expect("Could not read `activate.toml` file.");
-    
-    let toml: Value = toml::from_str(&contents).expect("Could not parse `activate.toml`.");
-
-    let environments_map: &toml::map::Map<String, Value> = toml
-        .as_table()
-        .expect("Root of `activate.toml` must be a table.");
-
-    let mut environments: Vec<Environment> = Vec::new();
-    for (env_name, env_data) in environments_map.into_iter() {
-        environments.push(Environment {
-            name: env_name.to_string(),
-            data: env_data.to_owned().try_into().expect("The table data does not follow the expect schema"),
-        });
-    }
+    let contents = fs::read_to_string(&activate_file)
+        .expect(&format!("Could not read `{}` file.", ACTIVATE_TOML));
+    let mut toml: Environments =
+        toml::from_str(&contents).expect(&format!("Could not parse `{}`.", ACTIVATE_TOML));
 
     let args: Activate = Activate::parse();
+    let selected_env = args.env_name;
+    let EnvironmentData { env } = toml
+        .0
+        .remove(&selected_env)
+        .expect(&format!("'{}' is not a valid environment", &selected_env));
 
-    decativate_old_env();
-    add_env_vars();
+    let activate_current_dir = Path::new(ACTIVATE_DIR);
+    if activate_current_dir.exists() {
+        decativate_old_env(&activate_current_dir);
+    } else {
+        fs::create_dir(activate_current_dir)
+            .expect(&format!("Could not create `{}` directory.", ACTIVATE_DIR));
+    }
+
+    let activate_env_file = fs::File::create(activate_current_dir.join(ENV_FILE))
+        .expect(&format!("Could not create `{}` file.", ENV_FILE));
+    add_env_vars(&env, &activate_env_file);
 }
 
 #[derive(Debug, Deserialize)]
-struct Environment {
-    name: String,
-    data: EnvironmentData,
-}
+struct Environments(HashMap<String, EnvironmentData>);
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct EnvironmentData {
-    env: HashMap<String,String>,
+    env: HashMap<String, String>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct ActiveEnvironmentEnv(HashMap<String, String>);
 
-#[derive(Debug, Deserialize)]
-struct EnvVar {
-    name: String,
-    value: String,
+fn decativate_old_env(activate_current_dir: &Path) {
+    let activate_env_file = activate_current_dir.join(ENV_FILE);
+    if !activate_env_file.exists() {
+        return;
+    }
+    let contents = fs::read_to_string(&activate_env_file)
+        .expect(&format!("Could not read `{}` file.", ENV_FILE));
+
+    let current_env: ActiveEnvironmentEnv =
+        serde_json::from_str(&contents).expect(&format!("Could not parse `{}`.", ENV_FILE));
+
+    remove_env_vars(&current_env.0);
+    fs::remove_file(&activate_env_file).expect(&format!(
+        "Could not remove `{}` file, but environment variables were deactivated. Please remove it manually.",
+        ENV_FILE
+    ));
 }
 
-fn decativate_old_env() {
-    // ...
+fn remove_env_vars(env_vars: &HashMap<String, String>) {
+    for (key, _value) in env_vars {
+        env::remove_var(key);
+    }
 }
 
-fn add_env_vars() {
-    // ...
+fn add_env_vars(env_vars: &HashMap<String, String>, mut activate_current_file: &File) {
+    for (key, value) in env_vars {
+        env::set_var(key, value);
+    }
+    activate_current_file
+        .write(
+            serde_json::to_string(&ActiveEnvironmentEnv(env_vars.clone()))
+                .expect("Could not serialize environment variables")
+                .as_bytes(),
+        )
+        .expect(&format!("Could not write to `{}` file.", ENV_FILE));
 }
